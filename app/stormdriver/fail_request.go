3ce3f6d2dd2fba3e8968638566af1d012fea0431
Michael Graff <explorer@flame.org>
2022-01-11 14:03:15 -0600

    split out fail request code

---
+++ app/stormdriver/fail_request.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func (s *srv) failAndLog() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		reqBody, err := io.ReadAll(req.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+			log.Printf("%v", err)
+			return
+		}
+		req.Body.Close()
+
+		t := tracer{
+			Method: req.Method,
+			Request: tracerHTTP{
+				Body:    base64.StdEncoding.EncodeToString(reqBody),
+				Headers: simplifyHeadersForLogging(req.Header),
+				URI:     req.RequestURI,
+			},
+		}
+		json, _ := json.Marshal(t)
+
+		log.Printf("%s", json)
+
+		// return not available for all of these
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}
+}
