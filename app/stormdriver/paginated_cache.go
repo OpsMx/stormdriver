dd74da1bdd24fcc8517b470f83330eb784eb5f70
Michael Graff <explorer@flame.org>
2022-01-27 19:39:31 -0600

    all but update done

--- app/stormdriver/paginated_cache.go
+++ app/stormdriver/paginated_cache.go
@@ -16,16 +16,16 @@
 
 package main
 
+import "fmt"
+
 // PaginatedCache holds the state and data for a cache that uses a specific format
 // of pagination.  Specifically, one that follows a model of a list of
 // paginated objects, where each member of this list has a page number and
 // items.
 type PaginatedCache struct {
-	// The cache.  Top level key is user+query.  Second level is platform.
-	cache       map[string]map[string][]cacheEntry
+	cache       map[string]*cacheEntry
 	updateChan  chan cacheUpdateResponse
 	requestChan chan CacheRequest
-	stopChan    chan bool
 }
 
 // CacheResponse is a reply to a CacheRequest.
@@ -47,29 +47,111 @@ type CacheResponse struct {
 // generally maps into this structure's shape.
 type CacheRequest struct {
 	Username     string
-	Query        string
+	QueryURL     string
 	PageNumber   int
 	PageSize     int
-	replyChannel chan CacheResponse
+	ReplyChannel chan CacheResponse
 }
 
+// cacheUpdateResponse contains the new data to store in the cache.
+// This is a complete update, with full data.
 type cacheUpdateResponse struct {
-	username string
-	query    string
-	items    []interface{}
+	username string        // copied from the request
+	queryURL string        // copied from the request
+	query    string        // from clouddriver
+	platform string        // from clouddriver
+	results  []interface{} // from clouddriver
 }
 
 // cacheEntry holds the data for a single query, scoped to the user by design.
 // States:
-//   If waitingClients is not empty, we have a fetch running.
-//   If waitingClients is empty, items is valid (even if empty), and we have no fetches running.
+// *  If waitingClients is not empty, we have a fetch running.
+// *  If waitingClients is empty, results is valid (even if empty), and
+//    we have no fetches running.
 type cacheEntry struct {
-	username       string
-	items          []interface{}
+	results        []interface{} // set from update
+	platform       string        // set from update
+	query          string        // set from update
 	expiry         int64
-	waitingClients []CacheRequest
+	waitingClients []*CacheRequest
 }
 
-func (c *PaginatedCache) runCache() {
+// MakePaginatedCache returns a new cache.
+func MakePaginatedCache() *PaginatedCache {
+	return &PaginatedCache{
+		cache:       map[string]*cacheEntry{},
+		updateChan:  make(chan cacheUpdateResponse),
+		requestChan: make(chan CacheRequest),
+	}
+}
 
+// RunCache runs the cache, forever.  Use a goroutine.
+func (c *PaginatedCache) RunCache() {
+	for {
+		select {
+		case request := <-c.requestChan:
+			key := fmt.Sprintf("%s::%s", request.Username, request.QueryURL)
+			entry, found := c.cache[key]
+			if !found {
+				go c.update(request.Username, request.QueryURL)
+				c.cache[key] = &cacheEntry{
+					expiry:         0,
+					waitingClients: []*CacheRequest{&request},
+				}
+				continue
+			}
+			if len(entry.waitingClients) == 0 {
+				c.reply(entry, &request)
+			} else {
+				entry.waitingClients = append(entry.waitingClients, &request)
+			}
+		case update := <-c.updateChan:
+			key := fmt.Sprintf("%s::%s", update.username, update.queryURL)
+			c.cache[key].results = update.results
+			c.cache[key].platform = update.platform
+			c.cache[key].query = update.query
+			for _, request := range c.cache[key].waitingClients {
+				c.reply(c.cache[key], request)
+			}
+			c.cache[key].waitingClients = []*CacheRequest{}
+		}
+	}
+}
+
+func (c *PaginatedCache) update(username string, queryURL string) {
+	// fire off parallel queries, combine them, and send a reply to the
+	// cache runner.
+
+	c.updateChan <- cacheUpdateResponse{
+		username: username,
+		queryURL: queryURL,
+		query:    "TODO",
+		platform: "TODO",
+		results:  []interface{}{},
+	}
+}
+
+func (c *PaginatedCache) reply(entry *cacheEntry, request *CacheRequest) {
+	startOffset := request.PageNumber * request.PageSize
+	endOffset := startOffset + request.PageSize
+
+	totalMatches := len(entry.results)
+
+	reply := CacheResponse{
+		TotalMatches: totalMatches,
+		PageNumber:   request.PageNumber,
+		PageSize:     request.PageSize,
+		Platform:     entry.platform,
+		Query:        entry.query,
+	}
+
+	if startOffset >= totalMatches {
+		reply.Results = []interface{}{}
+	} else {
+		if endOffset > totalMatches {
+			endOffset = totalMatches
+		}
+		reply.Results = entry.results[startOffset:endOffset]
+	}
+	request.ReplyChannel <- reply
 }
